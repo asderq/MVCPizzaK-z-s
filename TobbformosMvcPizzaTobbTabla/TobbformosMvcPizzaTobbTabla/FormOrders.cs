d9c91b8da44f9b4ad71e893e3dc51e764c65cc01
lázár martin <asderq007@gmail.com>
2020-02-05 13:30:34 +0100

    asd

--- TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/FormOrders.cs
+++ TobbformosMvcPizzaTobbTabla/TobbformosMvcPizzaTobbTabla/FormOrders.cs
@@ -31,6 +31,7 @@ namespace TobbformosMvcPizzaTobbTabla
 
             dgv.CellContentClick += Dgv_CellContentClick;
             dgv.DefaultValuesNeeded += Dgv_DefaultValuesNeeded;
+            dgv.EditingControlShowing += Dgv_EditingControlShowing;
 
             DataGridViewComboBoxColumn cbc = new DataGridViewComboBoxColumn();
             cbc.Name = "pizzaName";
@@ -78,6 +79,11 @@ namespace TobbformosMvcPizzaTobbTabla
 
       }
 
+        private void Dgv_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         private void Dgv_DefaultValuesNeeded(object sender, DataGridViewRowEventArgs e)
         {
             e.Row.Cells[3].Value = "1";
@@ -86,14 +92,27 @@ namespace TobbformosMvcPizzaTobbTabla
         private void Dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             Debug.WriteLine("dgv:" + e.RowIndex + "," + e.ColumnIndex);
-
             var senderGrid = (DataGridView)sender;
             if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn)
             {
-                if (e.ColumnIndex==1)
-                    Debug.WriteLine("dgv: + gomb" );
-                else if (e.ColumnIndex==2)
+                if (e.ColumnIndex == 1)
+                {
+                    Debug.WriteLine("dgv: + gomb");
+                    int db = Convert.ToInt32(dgv.Rows[e.RowIndex].Cells["mennyiseg"].Value.ToString());
+                    db = db + 1;
+                    dgv.Rows[e.RowIndex].Cells["mennyiseg"].Value = db;
+                }
+                else if (e.ColumnIndex == 2)
+                {
                     Debug.WriteLine("dgv: - gomb");
+                    int db = Convert.ToInt32(dgv.Rows[e.RowIndex].Cells["mennyiseg"].Value.ToString());
+                    if (db > 1)
+                    {
+                        db = db - 1;
+                    }
+                    dgv.Rows[e.RowIndex].Cells["mennyiseg"].Value = db;
+                }
+
             }
         }
     }
